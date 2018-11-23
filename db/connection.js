42ae44cd8cba7d72306e9277cef52cd32b6ff978
Michael-Holland-93 <mike.holland@btinternet.com>
2018-11-23 15:29:54 +0000

    heroku setup

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,8 @@
 const knex = require('knex');
-// const development_nc_news = require('./databases/development.sql');
-
-
-// module.exports = knex({
-//   client: 'pg',
-//   connection: 'postgres://localhost:5432/databases/development_nc_news',
-// });
 
 const ENV = process.env.NODE_ENV || 'development';
 
-const dbConfig = require('../knexfile')[ENV];
+const dbConfig = ENV === 'production' ? { client: 'pg', connection: process.env.DB_URL } : require('../knexfile')[ENV];
 
 const db = knex(dbConfig);
 
