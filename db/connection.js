c8e2fa1e3ef59a32faa510ec85dc01905d7952b8
Michael-Holland-93 <mike.holland@btinternet.com>
2018-11-23 16:04:57 +0000

    heroku

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 
 const ENV = process.env.NODE_ENV || 'development';
 
-const dbConfig = ENV === 'production' ? { client: 'pg', connection: process.env.DB_URL } : require('../knexfile')[ENV];
+const dbConfig = ENV === 'production' ? { client: 'pg', connection: process.env.DATABASE_URL } : require('../knexfile')[ENV];
 
 const db = knex(dbConfig);
 
